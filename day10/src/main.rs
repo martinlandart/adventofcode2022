5a2a3568efc988ae8e5609416cc3630f7e84468a
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2022-12-13 10:26:07 -0300

    day 10 part 1 done

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,7 +1,9 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./data").expect("failed to read file");
+
+    println!("total {}", get_cycle_totals(&contents));
 }
 #[derive(Debug)]
 enum Instruction {
@@ -9,20 +11,56 @@ enum Instruction {
     Noop,
 }
 
+enum Subroutine {
+    Sleep,
+    AddX(isize),
+}
+
 struct CPU {
     cycle: usize,
     register: isize,
+    stack: Vec<Subroutine>,
+}
+
+impl Iterator for CPU {
+    type Item = (usize, isize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cycle += 1;
+
+        if let Some(sr) = self.stack.pop() {
+            match sr {
+                Subroutine::AddX(v) => self.register += v,
+                Subroutine::Sleep => {}
+            }
+            Some((self.cycle, self.register))
+        } else {
+            None
+        }
+    }
 }
 
 impl CPU {
-    fn process(&mut self, instr: Instruction) {
-        self.cycle += 1;
-
+    fn push_instr(&mut self, instr: &Instruction) {
         match instr {
-            Instruction::AddX(v) => self.register += v,
-            Instruction::Noop => {}
+            Instruction::AddX(v) => {
+                self.stack.push(Subroutine::AddX(*v));
+                self.stack.push(Subroutine::Sleep);
+            }
+            Instruction::Noop => self.stack.push(Subroutine::Sleep),
         }
     }
+
+    // fn process(&mut self, sr: &Subroutine) {
+    //     self.cycle += 1;
+    //
+    //     // if let Some(sr) = self.stack.pop() {
+    //     match sr {
+    //         Subroutine::AddX(v) => self.register += v,
+    //         Subroutine::Sleep => {}
+    //     }
+    //     // };
+    // }
 }
 
 #[test]
@@ -30,45 +68,70 @@ fn instructions_test() {
     let mut cpu = CPU {
         cycle: 0,
         register: 1,
+        stack: vec![],
     };
 
-    cpu.process(Instruction::Noop);
-    assert_eq!(cpu.cycle, 1);
-
     assert_eq!(cpu.register, 1);
-    cpu.process(Instruction::AddX(15));
+    cpu.push_instr(&Instruction::Noop);
+    cpu.next();
+    assert_eq!(cpu.cycle, 1);
+    assert_eq!(cpu.register, 1);
+
+    cpu.push_instr(&Instruction::AddX(15));
+
+    cpu.next();
+    assert_eq!(cpu.cycle, 2);
+    assert_eq!(cpu.register, 1);
+
+    cpu.next();
+    assert_eq!(cpu.cycle, 3);
     assert_eq!(cpu.register, 16);
 }
 
-#[test]
-fn test_case() {
-    let contents = fs::read_to_string("./testdata").expect("failed to read file");
-
+fn get_cycle_totals(input: &str) -> isize {
     let mut cpu = CPU {
         cycle: 1,
         register: 1,
+        stack: vec![],
     };
-
-    let mut strengths = vec![];
-    contents
+    let instructions = input
         .lines()
         .map(|line| match line[..4].as_ref() {
             "noop" => Instruction::Noop,
             "addx" => Instruction::AddX(line[5..].parse::<isize>().unwrap()),
             _ => Instruction::Noop,
         })
-        .for_each(|instr| {
-            println!(
-                "instruction: {:?}, cycle:{}, register: {}",
-                instr, cpu.cycle, cpu.register
-            );
+        .rev()
+        .collect::<Vec<Instruction>>();
 
-            if cpu.register == 420 {
-                println!("hi");
-                strengths.push(cpu.register);
-            }
-            cpu.process(instr);
-        });
+    instructions.iter().for_each(|i| cpu.push_instr(i));
 
-    assert_eq!(strengths[0], 420);
+    let mut cycles: HashSet<usize> = HashSet::new();
+    cycles.insert(20);
+    cycles.insert(60);
+    cycles.insert(100);
+    cycles.insert(140);
+    cycles.insert(180);
+    cycles.insert(220);
+
+    let strengths = cpu.into_iter().fold(vec![], |mut accum: Vec<isize>, item| {
+        if cycles.contains(&item.0) {
+            accum.push(item.1 * item.0 as isize);
+        }
+        accum
+    });
+
+    let mut total = 0;
+    for s in strengths {
+        total += s;
+    }
+
+    total
+}
+
+#[test]
+fn test_case() {
+    let contents = fs::read_to_string("./testdata").expect("failed to read file");
+    let total = get_cycle_totals(&contents);
+    assert_eq!(total, 13140);
 }
