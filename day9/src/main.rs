aec6029739d8ccdb57ee80709077a2ce43899a87
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2022-12-12 22:56:05 -0300

    day 9 part 2 done

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,12 +6,22 @@ fn main() {
 
     let mut grid = Grid {
         head: Point(0, 0),
-        tail: Point(0, 0),
+        tail: vec![Point(0, 0); 9],
     };
 
+    println!(
+        "unique tail positions {}",
+        unique_tail_positions(&mut grid, &contents)
+    );
+}
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
+struct Point(isize, isize);
+
+fn unique_tail_positions(grid: &mut Grid, input: &str) -> usize {
     let mut map = HashSet::new();
 
-    contents
+    input
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .for_each(|line| {
@@ -30,59 +40,11 @@ fn main() {
                     _ => Direction::Left,
                 };
                 grid.move_dir(direction);
-                map.insert(grid.tail);
-            }
-        });
-
-    println!("unique tail positions {}", map.len());
-}
-
-#[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
-struct Point(isize, isize);
-
-fn unique_tail_positions(input: &str) -> usize {
-    let mut grid = Grid {
-        head: Point(0, 0),
-        tail: Point(0, 0),
-    };
-
-    let mut map = HashSet::new();
-
-    input
-        .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .for_each(|line| {
-            let times = line[2].to_digit(10).unwrap();
-
-            for _ in 0..times {
-                let direction = match line[0] {
-                    'L' => Direction::Left,
-                    'R' => Direction::Right,
-                    'D' => Direction::Down,
-                    'U' => Direction::Up,
-                    _ => Direction::Left,
-                };
-                grid.move_dir(direction);
-                map.insert(grid.tail);
+                map.insert(grid.tail[grid.tail.len() - 1]);
             }
         });
 
     map.len()
-}
-
-impl Point {
-    fn vertical_to(&self, p: &Point) -> bool {
-        self.1 == p.1
-    }
-    fn horizontal_to(&self, p: &Point) -> bool {
-        self.0 == p.0
-    }
-    fn diagonal_to(&self, p: &Point) -> bool {
-        let are_horizontal = self.0 == p.0;
-        let are_vertical = self.1 == p.1;
-
-        !are_horizontal && !are_vertical
-    }
 }
 
 fn are_touching(head: &Point, tail: &Point) -> bool {
@@ -95,7 +57,7 @@ fn are_touching(head: &Point, tail: &Point) -> bool {
 #[derive(Debug)]
 struct Grid {
     head: Point,
-    tail: Point,
+    tail: Vec<Point>,
 }
 
 impl Grid {
@@ -108,12 +70,30 @@ impl Grid {
             Direction::Down => self.head.0 -= 1,
         }
 
-        let tail = self.tail.into();
-
-        if !are_touching(&self.head, &tail) {
-            self.tail.0 += (self.head.0 - tail.0).signum() * 1;
-            self.tail.1 += (self.head.1 - tail.1).signum() * 1;
+        fn move_tails(head: &mut Point, tail: &mut Point) {
+            if !are_touching(&head, &tail) {
+                tail.0 += (&head.0 - &tail.0).signum() * 1;
+                tail.1 += (&head.1 - &tail.1).signum() * 1;
+            }
         }
+
+        fn move_tails_vec(idx: usize, tails: &mut Vec<Point>) {
+            let headIdx = idx;
+            let tailIdx = idx + 1;
+            if !are_touching(&tails[headIdx], &tails[tailIdx]) {
+                tails[tailIdx].0 += (&tails[headIdx].0 - &tails[tailIdx].0).signum() * 1;
+                tails[tailIdx].1 += (&tails[headIdx].1 - &tails[tailIdx].1).signum() * 1;
+            }
+        }
+
+        move_tails(&mut self.head, &mut self.tail[0]);
+
+        for i in 0..self.tail.len() - 1 {
+            move_tails_vec(i, &mut self.tail);
+        }
+
+        //
+        // move_tails(self.head, &mut self.tail);
     }
 }
 
@@ -146,7 +126,7 @@ fn are_touching_test() {
 fn visited_once_test() {
     let mut grid = Grid {
         head: Point(0, 0),
-        tail: Point(0, 0),
+        tail: vec![Point(0, 0)],
     };
 
     (0..4).for_each(|_| grid.move_dir(Direction::Right));
@@ -164,25 +144,47 @@ D 1
 L 5
 R 2";
 
-    assert_eq!(unique_tail_positions(input), 13);
+    let mut grid = Grid {
+        head: Point(0, 0),
+        tail: vec![Point(0, 0)],
+    };
+    assert_eq!(unique_tail_positions(&mut grid, input), 13);
+}
+
+#[test]
+fn multi_tailtest_case() {
+    let input = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
+    let mut grid = Grid {
+        head: Point(0, 0),
+        tail: vec![Point(0, 0); 9],
+    };
+    assert_eq!(unique_tail_positions(&mut grid, input), 36);
 }
 
 #[test]
 fn tail_follows() {
     let mut input = Grid {
         head: Point(0, 0),
-        tail: Point(0, 0),
+        tail: vec![Point(0, 0)],
     };
 
     (0..4).for_each(|_| input.move_dir(Direction::Right));
     (0..4).for_each(|_| input.move_dir(Direction::Up));
     assert_eq!(input.head, Point(4, 4));
-    assert_eq!(input.tail, Point(3, 4));
+    assert_eq!(input.tail[0], Point(3, 4));
 
     input.move_dir(Direction::Left);
     input.move_dir(Direction::Left);
     input.move_dir(Direction::Left);
 
     assert_eq!(input.head, Point(4, 1));
-    assert_eq!(input.tail, Point(4, 2));
+    assert_eq!(input.tail[0], Point(4, 2));
 }
